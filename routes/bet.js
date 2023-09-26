f2f7664614758095666c119d8ddf7537f69f4db5
jlamar27 <jlamar2712@gmail.com>
2023-09-26 13:50:20 -0400

    verifyAuth works and bet ctrl works

--- routes/bet.js
+++ routes/bet.js
@@ -1,14 +1,16 @@
-import express from 'express';
+import express from "express";
 const router = express.Router();
 
+import verifyAuth from "../middlewares/verifyAuth.js";
 
-import * as betController from '../controllers/bet.js';
+import * as betController from "../controllers/bet.js";
+
+// add verifyAuth to each
 
 // create a bet
-router.post('/', betController.createBet)
+router.post("/:userId", verifyAuth, betController.createBet);
 
 // get a single bet by its id
-router.get('/:id', betController.getSingleBet)
+router.get("/:userId/:id", verifyAuth, betController.getSingleBet);
 
-
-export default router
+export default router;
